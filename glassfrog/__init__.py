f470d25f68bd819e5f4932263a889d26fa6b0867
Ward Weistra <ward@thehyve.nl>
2016-05-27 00:15:24 +0200

    First successful notifications from installation and command

--- glassfrog/__init__.py
+++ glassfrog/__init__.py
@@ -4,6 +4,7 @@ import requests
 app = Flask(__name__)
 
 myserver = "http://5.157.82.115:45277"
+app.token = ''
 
 
 @app.route('/')
@@ -57,8 +58,35 @@ def installed():
     installdata = json.loads(request.get_data())
     # b'{"oauthId": "f3100c47-9936-40e8-a8aa-798b1e8da8f0", "capabilitiesUrl": "https://api.hipchat.com/v2/capabilities", "roomId": 2589171, "groupId": 46617, "oauthSecret": "Jgtf1Baj5KrSpXHZ7LbB0H3Krwr6cotrkQgkJm9C"}'
     print(installdata)
-    capabilitiesdata = requests.get(installdata['capabilitiesUrl'])
-    print(capabilitiesdata)
+    CLIENT_ID = installdata['oauthId']
+    CLIENT_SECRET = installdata['oauthSecret']
+    room_id = installdata['roomId']
+
+    capabilitiesdata = json.loads(requests.get(installdata['capabilitiesUrl']).text)
+    tokenUrl = capabilitiesdata['capabilities']['oauth2Provider']['tokenUrl']
+    print(tokenUrl)
+
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {"grant_type": "client_credentials",
+                 "scope": "send_notification"}
+    tokendata = json.loads(requests.post(tokenUrl,
+                                         auth=client_auth,
+                                         data=post_data).text)
+    print(tokendata)
+    # {'expires_in': 431999999, 'group_name': 'tranSMART', 'access_token': 'qjVBeM4ckCYzrc2prQMwuRZnHB3xUVsBwZISP0TF', 'group_id': 46617, 'scope': 'send_notification', 'token_type': 'bearer'}
+    app.token = tokendata['access_token']
+    apiUrl = capabilitiesdata['capabilities']['hipchatApiProvider']['url']
+    messageUrl = apiUrl+'/room/{}/notification'.format(room_id)
+    token_header = {"Authorization":"Bearer "+app.token}
+    data = {
+        "color": "green",
+        "message": "Installed (boris)",
+        "notify": False,
+        "message_format": "text"
+        }
+    messageresponse = requests.post(messageUrl,
+                                    headers=token_header,
+                                    data=data)
     return ('', 200)
 
 
@@ -66,10 +94,11 @@ def installed():
 def test():
     print(request.get_data())
     # b'{"event": "room_message", "item": {"message": {"date": "2016-05-26T15:32:43.700609+00:00", "from": {"id": 351107, "links": {"self": "https://api.hipchat.com/v2/user/351107"}, "mention_name": "WardWeistra", "name": "Ward Weistra", "version": "00000000"}, "id": "715f101f-1baa-4a5c-958a-9c6c7efaaa1f", "mentions": [], "message": "/test", "type": "message"}, "room": {"id": 2589171, "is_archived": false, "links": {"members": "https://api.hipchat.com/v2/room/2589171/member", "participants": "https://api.hipchat.com/v2/room/2589171/participant", "self": "https://api.hipchat.com/v2/room/2589171", "webhooks": "https://api.hipchat.com/v2/room/2589171/webhook"}, "name": "The Hyve - Holacracy", "privacy": "private", "version": "0XLIKALD"}}, "oauth_client_id": "ed8bb9f0-02d8-426b-9226-0d50fdcd47ea", "webhook_id": 4965523}'
+    token_header = {"Authorization":"Bearer "+app.token}
     message_dict = {
         "color": "green",
-        "message": "My first notification (yey)",
-        "notify": false,
+        "message": "Test! (natasha)",
+        "notify": False,
         "message_format": "text"
         }
     # return ('', 200)
