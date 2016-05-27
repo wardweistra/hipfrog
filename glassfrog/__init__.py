bc90927b257d01005a54c4cab40505d0d362c6f0
Ward Weistra <ward@thehyve.nl>
2016-05-27 23:10:20 +0200

    Some debugging

--- glassfrog/__init__.py
+++ glassfrog/__init__.py
@@ -100,6 +100,7 @@ def installed():
 def uninstall(oauthId):
     return ('', 200)
 
+
 def getcircles():
     headers = {'X-Auth-Token': app.glassfrogtoken}
     circlesUrl = 'https://glassfrog.holacracy.org/api/v3/circles'
@@ -113,6 +114,7 @@ def test():
     # b'{"event": "room_message", "item": {"message": {"date": "2016-05-26T15:32:43.700609+00:00", "from": {"id": 351107, "links": {"self": "https://api.hipchat.com/v2/user/351107"}, "mention_name": "WardWeistra", "name": "Ward Weistra", "version": "00000000"}, "id": "715f101f-1baa-4a5c-958a-9c6c7efaaa1f", "mentions": [], "message": "/test", "type": "message"}, "room": {"id": 2589171, "is_archived": false, "links": {"members": "https://api.hipchat.com/v2/room/2589171/member", "participants": "https://api.hipchat.com/v2/room/2589171/participant", "self": "https://api.hipchat.com/v2/room/2589171", "webhooks": "https://api.hipchat.com/v2/room/2589171/webhook"}, "name": "The Hyve - Holacracy", "privacy": "private", "version": "0XLIKALD"}}, "oauth_client_id": "ed8bb9f0-02d8-426b-9226-0d50fdcd47ea", "webhook_id": 4965523}'
     if app.glassfrogtoken != '':
         message = getcircles()
+        print(message)
         message_dict = {
             "color": "green",
             "message": message,
