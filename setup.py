93ab9347d7381ed2ae07b992760dc61692705177
Ward Weistra <ward@thehyve.nl>
2016-05-29 13:29:50 +0200

    First commit of setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+NAME = 'Glassfrog Hipchat Bot'
+VERSION = '0.1'
+mainscript = 'runserver.py'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='Hipchat extension to talk to Glassfrog',
+    url='https://github.com/wardweistra/glassfrog-hipchat-bot',
+    author='Ward Weistra',
+    author_email='ward@thehyve.nl',
+    license='GNU General Public License V3',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'],
+    scripts=[mainscript],
+)
