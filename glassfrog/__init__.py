47662dea50e7ee676833da88bbb9c2f65f0bbca4
Ward Weistra <ward@thehyve.nl>
2016-05-30 12:07:55 +0200

    Refactoring and message when configured successfully

--- glassfrog/__init__.py
+++ glassfrog/__init__.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 app.secret_key = 'not_so_secret'
 
 myserver = "http://5.157.82.115:45277"
-app.token = ''
-app.glassfrogtoken = ''
+app.hipchatToken = ''
+app.glassfrogToken = ''
+app.hipchatApiUrl = ''
+app.roomId = ''
 
 
 @app.route('/')
@@ -69,7 +71,7 @@ def installed():
         print(installdata)
         CLIENT_ID = installdata['oauthId']
         CLIENT_SECRET = installdata['oauthSecret']
-        room_id = installdata['roomId']
+        app.roomId = installdata['roomId']
 
         capabilitiesdata = json.loads(requests.get(installdata['capabilitiesUrl']).text)
         tokenUrl = capabilitiesdata['capabilities']['oauth2Provider']['tokenUrl']
@@ -83,19 +85,10 @@ def installed():
                                              data=post_data).text)
         print(tokendata)
         # {'expires_in': 431999999, 'group_name': 'tranSMART', 'access_token': 'qjVBeM4ckCYzrc2prQMwuRZnHB3xUVsBwZISP0TF', 'group_id': 46617, 'scope': 'send_notification', 'token_type': 'bearer'}
-        app.token = tokendata['access_token']
-        apiUrl = capabilitiesdata['capabilities']['hipchatApiProvider']['url']
-        messageUrl = apiUrl+'/room/{}/notification'.format(room_id)
-        token_header = {"Authorization": "Bearer "+app.token}
-        data = {
-            "color": "green",
-            "message": "Installed successfully. Type /hola to get started.",
-            "notify": False,
-            "message_format": "text"
-            }
-        messageresponse = requests.post(messageUrl,
-                                        headers=token_header,
-                                        data=data)
+        app.hipchatToken = tokendata['access_token']
+        app.hipchatApiUrl = capabilitiesdata['capabilities']['hipchatApiProvider']['url']
+
+        sendMessage('green', "Installed successfully. Please set Glassfrog Token in the Hipchat Integration Configure page.")
     return ('', 200)
 
 
@@ -106,7 +99,7 @@ def uninstall(oauthId):
 
 
 def getCircles():
-    headers = {'X-Auth-Token': app.glassfrogtoken}
+    headers = {'X-Auth-Token': app.glassfrogToken}
     circlesUrl = 'https://glassfrog.holacracy.org/api/v3/circles'
     circlesresponse = requests.get(circlesUrl, headers=headers)
     print(circlesresponse)
@@ -117,42 +110,52 @@ def getCircles():
     return code, circles
 
 
+def createMessageDict(color, message):
+    message_dict = {
+        "color": color,
+        "message": message,
+        "notify": False,
+        "message_format": "text"
+        }
+    return message_dict
+
+
+def sendMessage(color, message):
+    messageUrl = app.hipchatApiUrl+'/room/{}/notification'.format(app.roomId)
+    token_header = {"Authorization": "Bearer "+app.hipchatToken}
+    data = createMessageDict(color, message)
+    messageresponse = requests.post(messageUrl,
+                                    headers=token_header,
+                                    data=data)
+
+
 @app.route('/hola', methods=['GET', 'POST'])
 def hola():
     print(request.get_data())
     # b'{"event": "room_message", "item": {"message": {"date": "2016-05-26T15:32:43.700609+00:00", "from": {"id": 351107, "links": {"self": "https://api.hipchat.com/v2/user/351107"}, "mention_name": "WardWeistra", "name": "Ward Weistra", "version": "00000000"}, "id": "715f101f-1baa-4a5c-958a-9c6c7efaaa1f", "mentions": [], "message": "/test", "type": "message"}, "room": {"id": 2589171, "is_archived": false, "links": {"members": "https://api.hipchat.com/v2/room/2589171/member", "participants": "https://api.hipchat.com/v2/room/2589171/participant", "self": "https://api.hipchat.com/v2/room/2589171", "webhooks": "https://api.hipchat.com/v2/room/2589171/webhook"}, "name": "The Hyve - Holacracy", "privacy": "private", "version": "0XLIKALD"}}, "oauth_client_id": "ed8bb9f0-02d8-426b-9226-0d50fdcd47ea", "webhook_id": 4965523}'
-    if app.glassfrogtoken != '':
+    if app.glassfrogToken != '':
         code, message = getCircles()
         print(message)
-        message_dict = {
-            "color": "green",
-            "message": message,
-            "notify": False,
-            "message_format": "text"
-            }
+        message_dict = createMessageDict('green', message)
     else:
-        message_dict = {
-            "color": "red",
-            "message": "Please set the Glassfrog token first in the plugin configuration",
-            "notify": False,
-            "message_format": "text"
-            }
+        message_dict = createMessageDict('red', "Please set the Glassfrog token first in the plugin configuration")
     return json.jsonify(message_dict)
 
 
 @app.route('/configure.html', methods=['GET', 'POST'])
 def configure():
     if request.method == 'POST':
-        app.glassfrogtoken = request.form['glassfrogtoken']
         code, message = getCircles()
         if code == 200:
+            app.glassfrogToken = request.form['glassfrogtoken']
             flashmessage = 'Valid Glassfrog Token stored'
+            sendMessage('green', "Configured successfully. Type /hola to get started!")
         else:
             flashmessage = 'Encountered Error '+str(code)+' when testing the Glassfrog Token.'
             if 'message' in message:
                 flashmessage = flashmessage + ' Message given: \''+message['message']+'\'.'
         flash(flashmessage)
-    return render_template('configure.html', glassfrogtoken=app.glassfrogtoken)
+    return render_template('configure.html', glassfrogtoken=app.glassfrogToken)
 
 if __name__ == '__main__':
     app.run()
