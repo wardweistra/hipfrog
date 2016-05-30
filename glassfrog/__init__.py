fb70689468b0883ad8c8b3dd5c929667a01e22a0
Ward Weistra <ward@thehyve.nl>
2016-05-30 12:31:40 +0200

    Making messages str and some :fish:

--- glassfrog/__init__.py
+++ glassfrog/__init__.py
@@ -67,15 +67,13 @@ def capabilities():
 def installed():
     if request.method == 'POST':
         installdata = json.loads(request.get_data())
-        # b'{"oauthId": "f3100c47-9936-40e8-a8aa-798b1e8da8f0", "capabilitiesUrl": "https://api.hipchat.com/v2/capabilities", "roomId": 2589171, "groupId": 46617, "oauthSecret": "Jgtf1Baj5KrSpXHZ7LbB0H3Krwr6cotrkQgkJm9C"}'
-        print(installdata)
+
         CLIENT_ID = installdata['oauthId']
         CLIENT_SECRET = installdata['oauthSecret']
         app.roomId = installdata['roomId']
 
         capabilitiesdata = json.loads(requests.get(installdata['capabilitiesUrl']).text)
         tokenUrl = capabilitiesdata['capabilities']['oauth2Provider']['tokenUrl']
-        print(tokenUrl)
 
         client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
         post_data = {"grant_type": "client_credentials",
@@ -83,8 +81,7 @@ def installed():
         tokendata = json.loads(requests.post(tokenUrl,
                                              auth=client_auth,
                                              data=post_data).text)
-        print(tokendata)
-        # {'expires_in': 431999999, 'group_name': 'tranSMART', 'access_token': 'qjVBeM4ckCYzrc2prQMwuRZnHB3xUVsBwZISP0TF', 'group_id': 46617, 'scope': 'send_notification', 'token_type': 'bearer'}
+
         app.hipchatToken = tokendata['access_token']
         app.hipchatApiUrl = capabilitiesdata['capabilities']['hipchatApiProvider']['url']
 
@@ -113,7 +110,7 @@ def getCircles():
 def createMessageDict(color, message):
     message_dict = {
         "color": color,
-        "message": message,
+        "message": str(message),
         "notify": False,
         "message_format": "text"
         }
@@ -132,7 +129,6 @@ def sendMessage(color, message):
 @app.route('/hola', methods=['GET', 'POST'])
 def hola():
     print(request.get_data())
-    # b'{"event": "room_message", "item": {"message": {"date": "2016-05-26T15:32:43.700609+00:00", "from": {"id": 351107, "links": {"self": "https://api.hipchat.com/v2/user/351107"}, "mention_name": "WardWeistra", "name": "Ward Weistra", "version": "00000000"}, "id": "715f101f-1baa-4a5c-958a-9c6c7efaaa1f", "mentions": [], "message": "/test", "type": "message"}, "room": {"id": 2589171, "is_archived": false, "links": {"members": "https://api.hipchat.com/v2/room/2589171/member", "participants": "https://api.hipchat.com/v2/room/2589171/participant", "self": "https://api.hipchat.com/v2/room/2589171", "webhooks": "https://api.hipchat.com/v2/room/2589171/webhook"}, "name": "The Hyve - Holacracy", "privacy": "private", "version": "0XLIKALD"}}, "oauth_client_id": "ed8bb9f0-02d8-426b-9226-0d50fdcd47ea", "webhook_id": 4965523}'
     if app.glassfrogToken != '':
         code, message = getCircles()
         print(message)
