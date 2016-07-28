6fb2368e6ba24503bf6fc7d4652e1ed1c6eb24d6
Ward Weistra <ward@thehyve.nl>
2016-07-27 20:35:28 -0400

    Removing cards

--- glassfrog/functions/messageFunctions.py
+++ glassfrog/functions/messageFunctions.py
@@ -3,52 +3,14 @@ from glassfrog.models import Installation
 from flask import escape
 
 
-def createMessageDict(color, message, card=None):
+def createMessageDict(color, message):
     message_dict = {
         "color": color,
         "message": str(message),
         "notify": False,
         "message_format": "html"
         }
-    if card is not None:
-        message_dict['card'] = card
     return message_dict
-
-
-def createMessageCard(url, title, description, icon=None, attributes=None):
-    message_card = {
-      "style": "application",
-      "url": url,
-      "format": "medium",
-      "id": "whatdoesthisiddo",
-      "title": str(title),
-      "description": {
-          "format": "html",
-          "value": str(description)
-        }
-    }
-    #   "icon": {
-    #     "url": "http://bit.ly/1S9Z5dF"
-    #   }
-    #   "attributes": [
-    #     {
-    #       "label": "attribute1",
-    #       "value": {
-    #         "label": "value1"
-    #       }
-    #     },
-    #     {
-    #       "label": "attribute2",
-    #       "value": {
-    #         "icon": {
-    #           "url": "http://bit.ly/1S9Z5dF"
-    #         },
-    #         "label": "value2",
-    #         "style": "lozenge-complete"
-    #       }
-    #     }
-    #   ]
-    return message_card
 
 
 def getInstallationFromJWT(signed_request):
