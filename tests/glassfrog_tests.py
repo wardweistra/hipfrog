c3a2b13f79bb8fa7aa308d064c37cd72fb04ee49
Ward Weistra <ward@thehyve.nl>
2017-09-07 00:21:23 +0200

    Fix for PEP

--- tests/glassfrog_tests.py
+++ tests/glassfrog_tests.py
@@ -121,7 +121,7 @@ class GlassfrogTestCase(unittest.TestCase):
 
         rv = hipchatApiHandler.getRoomMembers(installation=mock_installation)
         mock_requestUrl = '{}/room/{}/participant'.format(mock_installation.hipchatApiProvider_url,
-                                                     mock_installation.roomId)
+                                                          mock_installation.roomId)
         mock_token_header = {"Authorization": "Bearer "+mock_installation.access_token}
 
         mock_requests_get.assert_called_with(mock_requestUrl,
